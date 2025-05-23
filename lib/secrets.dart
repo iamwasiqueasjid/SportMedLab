bbf6a39507978bdddb34adf372fcfebddeacf259
AliHere76 <mu.ali.7645@gmail.com>
2025-05-23 23:02:35 +0500

    Remove secrets.dart from version control

---
+++ lib/secrets.dart
@@ -0,0 +1,13 @@
+final FIREBASE_ANDROID_API_KEY = 'AIzaSyAj1_w04Xo1M5UWrqedF9sj3ZrW5ZYeA38';
+final FIREBASE_ANDROID_APP_ID = '1:200118990270:android:2747c61b92b769224db5ef';
+final FIREBASE_ANDROID_MESSAGING_SENDER_ID = '200118990270';
+final FIREBASE_ANDROID_PROJECT_ID = 'sports-med-lab-4f8aa';
+final FIREBASE_ANDROID_STORAGE_BUCKET =
+    'sports-med-lab-4f8aa.firebasestorage.app';
+
+final FIREBASE_IOS_API_KEY = 'AIzaSyC5Pdmum1hMmTbg235i_n8WapjY7bgZfXU';
+final FIREBASE_IOS_APP_ID = '1:200118990270:ios:56eac0ec910a5eab4db5ef';
+final FIREBASE_IOS_MESSAGING_SENDER_ID = '200118990270';
+final FIREBASE_IOS_PROJECT_ID = 'sports-med-lab-4f8aa';
+final FIREBASE_IOS_STORAGE_BUCKET = 'sports-med-lab-4f8aa.firebasestorage.app';
+final FIREBASE_IOS_BUNDLE_ID = 'com.example.testProject';
